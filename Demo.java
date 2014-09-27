130e2c36ce40cbf79b533bb59fca12e9c43cf8f4
Ernesto Mancebo <ernesmancebo@gmail.com>
2014-09-27 15:57:34 -0400

    testing with simulator

---
+++ Demo.java
@@ -0,0 +1,14 @@
+class Demo {
+    public static void main(String args[]) {
+        int c = 0;
+        A: for (int i = 0; i < 2; i++) {
+            B: for (int j = 0; j < 2; j++) {
+                C: for (int k = 0; k < 3; k++) {
+                    c++;
+                    if (k > j) break;
+                }
+            }
+        }
+        System.out.println(c);
+    }
+}
